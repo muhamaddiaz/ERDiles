96198900866a7196af5feba9715e5efdb68d0986
captain_spirit <muhamaddiaz10@gmail.com>
2018-11-30 08:00:12 +0700

    ssl dan push heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,5 +28,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+
+        if(env('APP_ENV') === 'production') {
+            $this->app['url']->forceScheme('https');
+        }
     }
 }
